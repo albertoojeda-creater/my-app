5b5dd0a7429022ae4b8509bf0e1ef527c3a4ccd1
albertoojeda-creater <alberto.ojeda.ao2001@gmail.com>
2024-10-10 10:32:42 -0600

    Cambios del 10 Oct

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Script from "next/script";
 //import "@/app/style.css"
 
 export const metadata={
@@ -12,6 +13,8 @@ export default function RootLayout({children}){
             <body>
                 <Navbar/>
                 {children}
+                <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></Script>
+                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></Script>
             </body>
         </html>
     );
--- src/app/universidades/page.jsx
+++ src/app/universidades/page.jsx
@@ -1,8 +1,37 @@
-export default function Uni() {
+import axios from "axios";
+
+async function getNoticias() {
+    const url = "http://universities.hipolabs.com/search?country=Mexico";
+    const universidades = await axios.get(url);
+    //console.log(universidades);
+    return universidades.data;
+}
+
+export default async function Uni() {
+    const universidades = await getNoticias();
     return(
         <>
             <h1>Universidades</h1>
-            <p>Estas en universidades</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Universidad</th>
+                        <th>Url</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        universidades.map((universidad,i)=>(
+                            <tr key={i}>
+                                <td>{i+1}</td>
+                                <td>{universidad.name}</td>
+                                <td>{universidad.web_pages[0]}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </>
     );
 }
